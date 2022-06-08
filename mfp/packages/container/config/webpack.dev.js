68aa7f42ff2c73f5dfffd3409edee885d57004c7
Cole Sulak <nsulak@teksystems.com>
2022-06-08 13:19:40 -0500

    Finalized dashboard

--- mfp/packages/container/config/webpack.dev.js
+++ mfp/packages/container/config/webpack.dev.js
@@ -8,19 +8,19 @@ const devConfig = {
   output: {
     publicPath: 'http://localhost:8080/',
   },
-  devServer: { 
+  devServer: {
     port: 8080,
     historyApiFallback: {
       index: 'index.html',
     },
-  }, 
+  },
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
       remotes: {
         marketing: 'marketing@http://localhost:8081/remoteEntry.js',
         auth: 'auth@http://localhost:8082/remoteEntry.js',
-        // dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
       },
       shared: packageJson.dependencies,
     }),
